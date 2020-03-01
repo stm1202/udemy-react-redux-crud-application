146473c580ed40e0c61ef2242050828c751a0b68
stm1202 <stmescape@gmail.com>
2020-03-01 20:58:17 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 // class App extends Component {
 //   render() {
@@ -12,18 +12,28 @@ import React, {Component} from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Jiro"}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profiles, index) => {
+          return <User name={profiles.name} age={profiles.age} key={index} />
+        })
+      }
     </div>
   )
-} 
+}
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age} years old!!</div>
+}
+
+User.defaultProps = {
+  age:1
 }
 
 export default App;
