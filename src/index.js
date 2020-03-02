00e510f3f1a8be381db675b1cee9dea9da5506bc
stm1202 <stmescape@gmail.com>
2020-03-02 23:54:17 +0900

    imprement action and action creator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//アクションクリエーターの作成
+export const INCREMENT = 'INCREMENT'
+export const INCREMENT = 'DECREMENT'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export const increment = () => ({
+    type: 'INCREMENT'
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
