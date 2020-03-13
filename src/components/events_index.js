21abc63ba688296c7e3631be3fb3e630feee9cc0
stm1202 <stmescape@gmail.com>
2020-03-13 13:29:35 +0900

    apply material-ui to eventsIndex

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -3,6 +3,17 @@ import { connect } from 'react-redux'
 import _ from 'lodash';
 import { readEvents } from '../actions'
 import { Link } from 'react-router-dom'
+import {
+  Table,
+  TableHeader,
+  TableHeaderColumn,
+  TableBody,
+  TableFooter,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import FoatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 class EventsIndex extends Component {
   componentDidMount() {
@@ -11,33 +22,46 @@ class EventsIndex extends Component {
 
   renderEvents() {
     return _.map(this.props.events, event => (
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableRowColumn>{event.id}</TableRowColumn>
+        <TableRowColumn>
           <Link to={`/events/${event.id}`}>
             {event.title}
           </Link>
-        </td>
-        <td>{event.body}</td>
-      </tr>
+        </TableRowColumn>
+        <TableRowColumn>{event.body}</TableRowColumn>
+      </TableRow>
     ))
   }
   render() {
+    const style = {
+      position: "fixed",
+      right: 12,
+      bottom: 12
+    }
+
+
     return (
       <React.Fragment>
-        <tabel>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
+        <FoatingActionButton style={style} containerElement={<Link to="/events/new" />}>
+          <ContentAdd />
+        </FoatingActionButton>
+        <Table>
+          <TableHeader
+            displaySelectAll={false}
+            adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Title</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
 
-          <tbody>
+          <TableBody displayRowCheckbox={false}>
             {this.renderEvents()}
-          </tbody>
-        </tabel>
+          </TableBody>
+        </Table>
 
         <Link to="/events/new">New Event</Link>
 
