4788295272fe37fd12210f0d776200c75e89b26b
stm1202 <stmescape@gmail.com>
2020-03-13 13:56:53 +0900

    aply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { getEvents, deleteEvent, putEvents } from '../actions'
 import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 class EventsShow extends Component {
 
@@ -21,10 +23,14 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>)
+      <TextField
+      hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+    />)
   }
 
   async onDeleteClick(){
@@ -42,16 +48,13 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid }   = this.props
-
+    const style = { margin: 12 }
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/">Cancel</Link>
-          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
-        </div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid}/>
+        <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
       </form>
     )
   }
@@ -71,6 +74,7 @@ const validate = values => {
   if (!values.body) errors.body = "Enter a body, please."
   return errors
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({ validate, form: 'eventShowForm', enableReinitialize:true })(EventsShow)
 )
